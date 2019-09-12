0bf888ee3e570cf6f2892500f09f139c556943b3
am <amrm@cin.ufpe.br>
2019-09-11 22:47:59 -0300

    pagamento json e handlers

--- comandos/pagar.js
+++ comandos/pagar.js
@@ -1,7 +1,19 @@
 const request = require('request');
+
 const util = require('../util/util.js');
+   /* src="https://www.mercadopago.com.br/integrations/v1/web-tokenize-checkout.js"
+    data-public-key="ENV_PUBLIC_KEY"
+    data-transaction-amount="100.00"> */
+
+//Os campos obrigatórios para envio são o token, transaction_amount, payment_method_id e o payer.email.
 
 exports.run = async (client, message) => {
+    let data = JSON.parse()
+
+}
+
+
+/*exports.run = async (client, message) => {
     let url = 'https://api.hgbrasil.com/finance?format=json&key=a67d9b60';
     request(url, (err, response, body) => {
         if(err) console.log(err);
@@ -26,4 +38,57 @@ exports.run = async (client, message) => {
             }});
         }
     });
+}*/
+
+
+
+
+
+
+
+
+
+function getBin() {
+  const cardnumber = document.getElementById("cardnumber");
+  return cardnumber.substring(0,6);
 }
+
+function guessingPaymentMethod(event) {
+    var bin = getBin();
+
+    if (event.type == "keyup") {
+        if (bin.length >= 6) {
+            window.Mercadopago.getPaymentMethod({
+                "bin": bin
+            }, setPaymentMethodInfo);
+        }
+    } else {
+        setTimeout(function() {
+            if (bin.length >= 6) {
+                window.Mercadopago.getPaymentMethod({
+                    "bin": bin
+                }, setPaymentMethodInfo);
+            }
+        }, 100);
+    }
+};
+
+function setPaymentMethodInfo(status, response) {
+    if (status == 200) {
+        const paymentMethodElement = document.querySelector('input[name=paymentMethodId]');
+
+        if (paymentMethodElement) {
+            paymentMethodElement.value = response[0].id;
+        } else {
+            const input = document.createElement('input');
+            input.setattribute('name', 'paymentMethodId');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('value', response[0].id);     
+
+            form.appendChild(input);
+        }
+    } else {
+        alert(`payment method info error: ${response}`);  
+    }
+};
+
